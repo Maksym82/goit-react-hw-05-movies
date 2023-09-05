46e9b1515417cac3c0d8e64cae1b4e9b092843ea
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-05 16:28:29 +0300

    create Layout

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,25 @@
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+
+
+
+function Layout() {
+    return (
+        <div>
+            <Header>
+                <Navi>
+                    <StyledLink to='/'>Home</StyledLink>
+                    <StyledLink to='/movies'>Movies</StyledLink>
+                </Navi>
+            </Header>
+            <Main>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Outlet />
+                </Suspense>
+            </Main>
+        </div>
+    )
+}
+
+
+export default Layout;
