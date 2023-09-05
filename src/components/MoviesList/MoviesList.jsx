913d9321a0c93cd27469bfbbf4ac60931e2fad7c
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-05 21:01:42 +0300

    fix

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,5 +1,6 @@
 import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { List, MovieLink } from './MovieList.styled';
 
 function MoviesList({ movies }) {
   const location = useLocation();
