81f6df8904164f9f2673053f8613c3bbe5d6146f
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-05 21:35:53 +0300

    fix

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchAddition } from '../../services/api';
-import photo from './photo.jpg';
+import photo from './photo.png';
 import { CastList, CastItem, CastText, Name } from './Cast.styled';
 
 function Cast() {
